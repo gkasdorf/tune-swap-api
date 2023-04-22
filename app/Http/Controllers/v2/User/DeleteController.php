258b3720b9ae5e53810023448d137fe5785799c9
Gavin Kasdorf <me@gkasdorf.com>
2023-04-21 17:46:06 -0800

    don't require password for account deletion.

--- app/Http/Controllers/v2/User/DeleteController.php
+++ app/Http/Controllers/v2/User/DeleteController.php
@@ -4,20 +4,11 @@ namespace App\Http\Controllers\v2\User;
 
 use App\Helpers\ApiResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class DeleteController extends \App\Http\Controllers\Controller
 {
     public function delete(Request $request)
     {
-        $request->validate([
-            "password" => "required"
-        ]);
-
-        if (!Hash::check($request->password, $request->user()->password)) {
-            return ApiResponse::fail("Incorrect password.", 401);
-        }
-
         $request->user()->delete();
 
         return ApiResponse::success();
