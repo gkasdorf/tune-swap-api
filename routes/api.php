b9ebe8b6051962c1e54c611a7bbeaf8c7c826046
Gavin Kasdorf <me@gkasdorf.com>
2023-05-09 01:29:44 -0800

    remove v1 routes

--- routes/api.php
+++ routes/api.php
@@ -95,82 +95,8 @@ Route::middleware("auth:sanctum")->group(function () {
 });
 
 Route::get("/v2/share/{id}", [ShareController::class, "get"]);
-
-
-// V1 ROUTES DEPRECATED
-
-/*
- * User routes
- */
-
-Route::middleware("auth:sanctum")->get("/user/verify", [UserController::class, "verify"]);
-
-Route::post("/user/register", [UserController::class, "store"]);
-Route::post("/user/auth", [UserController::class, "auth"]);
-Route::get("/user/verify", [UserController:: class, "verify"]);
-
-
-Route::get("/applemusic/authPage", [AppleMusicAuthController::class, "authPage"]);
-Route::get("/applemusic/auth", [AppleMusicAuthController::class, "auth"]);
-
 /*
  * Settings routes
  */
 Route::get("/settings/maintenance", [SettingsController::class, "getMaintenance"]);
 
-Route::middleware("auth:sanctum")->group(function () {
-    /*
-     * User auth routes
-     */
-    Route::get("/user/has/spotify", [UserController::class, "hasSpotify"]);
-    Route::get("/user/has/applemusic", [UserController::class, "hasAppleMusic"]);
-    Route::get("/user/has/tidal", [UserController::class, "hasTidal"]);
-
-    Route::get("/user/notifications/ios/enabled", [UserController::class, "iosNotificationsEnabled"]);
-    Route::get("/user/notifications/ios/enable", [UserController::class, "enableIosNotifications"]);
-    Route::get("/user/notifications/ios/disable", [UserController::class, "disableIosNotifications"]);
-
-    Route::post("/user/delete", [UserController::class, "delete"]);
-
-    /*
-     * Spotify auth routes
-     */
-    Route::get("/spotify/authUrl", [SpotifyAuthController::class, "authUrl"]);
-    Route::get("/spotify/auth", [SpotifyAuthController::class, "auth"]);
-    Route::get("/spotify/authUrl/mobile", [SpotifyAuthController::class, "authUrlMobile"]);
-
-    /*
-     * Spotify user routes
-     */
-    Route::get("/spotify/me/playlists", [SpotifyController::class, "myPlaylists"]);
-    Route::get("/spotify/me/playlist/{id}", [SpotifyController::class, "playlist"]);
-    Route::get("/spotify/playlist/{id}", [SpotifyController::class, "playlist"]);
-    Route::get("/spotify/me/library", [SpotifyController::class, "library"]);
-
-    /*
-     * Apple Music routes
-     */
-    Route::get("/applemusic/me/library", [AppleMusicController::class, "library"]);
-    Route::get("/applemusic/playlist/{id}", [AppleMusicController::class, "playlist"]);
-    Route::get("/applemusic/me/playlist/{id}", [AppleMusicController::class, "userPlaylist"]);
-    Route::get("/applemusic/me/playlist/{id}/name", [AppleMusicController::class, "userPlaylistName"]);
-    Route::get("/applemusic/me/playlists", [AppleMusicController::class, "userPlaylists"]);
-    Route::get("/applemusic/me/storefront", [AppleMusicController::class, "storefront"]);
-
-    /**
-     * Tidal Routes
-     */
-    Route::get("/tidal/authUrl", [TidalController::class, "authUrl"]);
-    Route::post("/tidal/auth", [TidalController::class, "auth"]);
-
-    Route::get("/tidal/me/playlists", [TidalController::class, "playlists"]);
-    Route::get("/tidal/me/playlists/{id}", [TidalController::class, "playlist"]);
-    Route::get("/tidal/me/library", [TidalController::class, "library"]);
-
-    /*
-     * Swap routes
-     */
-    Route::post("/swap/start", [SwapController::class, "start"]);
-    Route::get("/swap", [SwapController::class, "swaps"]);
-    Route::get("/swap/{id}", [SwapController::class, "swap"]);
-});
